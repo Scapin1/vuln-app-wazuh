b9c645559c64de12e64eb6ed16cc7b4e4bc1ee94
claubells <claudia.villa@usach.cl>
2026-03-08 14:43:49 -0300

    se quita un comentario

--- frontend/src/presentation/views/ConfigWazuh.vue
+++ frontend/src/presentation/views/ConfigWazuh.vue
@@ -105,7 +105,7 @@
             </div>
           </div>
           <div class="form-group">
-            <label class="form-label">Contraseña Wazuh <span v-if="isEditing" style="font-weight:400; color:var(--text-muted);">(Vacío para mantener)</span></label>
+            <label class="form-label">Contraseña Wazuh <span v-if="isEditing" style="font-weight:400; color:var(--text-muted);"></span></label>
             <div class="input-icon">
               <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
               <input :type="showPassword ? 'text' : 'password'" v-model="newConn.wazuh_password" class="form-input" :required="!isEditing" placeholder="••••••••">
